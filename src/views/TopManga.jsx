e3bdd8e6055fc8f7d2d12dc43147987b5cac7ea0
Andrea <andrea.berrocal95@gmail.com>
2022-08-01 17:34:40 -0700

    test

--- src/views/TopManga.jsx
+++ src/views/TopManga.jsx
@@ -1,18 +1,17 @@
 import { useContext, useEffect } from "react";
-import { getData } from '../actions/Actions';
-import AddToList from '../components/AddToList';
-import ViewMoreInfo from '../components/ViewMoreInfo';
+import { getData } from "../actions/Actions";
+import AddToList from "../components/AddToList";
+import ViewMoreInfo from "../components/ViewMoreInfo";
 import { DataContext } from "../context/DataContext";
 
 const AllAnime = () => {
+  const { loading, error, data, dispatch } = useContext(DataContext);
 
-    const { loading, error, data, dispatch } = useContext(DataContext);
-
-    useEffect(() => {
+  useEffect(() => {
     dispatch({ type: "FETCHING" });
     const fetchData = async () => {
       try {
-        const allManga = await getData(`https://api.jikan.moe/v4/top/manga`);;
+        const allManga = await getData(`https://api.jikan.moe/v4/top/manga`);
         dispatch({ type: "FETCHED", payload: allManga });
       } catch (error) {
         dispatch({ type: "FETCH_ERROR" });
@@ -20,23 +19,32 @@ const AllAnime = () => {
     };
     fetchData();
     console.log("i am used once");
-    console.log(data)
-    }, [])
+    console.log(data);
+  }, []);
 
-    return(
-        <div className="container">
-                {loading? <img className="loading" src={require('../loading.webp')} alt='loader' /> 
-                : !loading && error? <div>{error}</div> 
-                : data.map((manga) => {
-                    return (
-                    <div key={manga.mal_id} className="card-container">
-                        <img src={manga.images.jpg.image_url} alt={manga.title} />
-                        <h1>{manga.title}</h1>
-                        <AddToList />
-                        <ViewMoreInfo />
-                    </div>
-                )})}
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      {loading ? (
+        <img
+          className="loading"
+          src={require("../loading.webp")}
+          alt="loader"
+        />
+      ) : !loading && error ? (
+        <div>{error}</div>
+      ) : (
+        data.map((manga) => {
+          return (
+            <div key={manga.mal_id} className="card-container">
+              <img src={manga.images.jpg.image_url} alt={manga.title} />
+              <h1>{manga.title}</h1>
+              <AddToList />
+              <ViewMoreInfo />
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+};
 export default AllAnime;
