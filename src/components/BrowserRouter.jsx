a267e7dbd9dabe41f3a03df860b323679867279d
Andrea <andrea.berrocal95@gmail.com>
2022-08-02 13:12:50 -0700

    added routes to router

--- src/components/BrowserRouter.jsx
+++ src/components/BrowserRouter.jsx
@@ -4,6 +4,8 @@ import TopAnime from "../views/TopAnime";
 import Home from "../views/Home";
 import AnimeRec from "../views/AnimeRec";
 import MangaRec from "../views/MangaRec";
+import RandomAnime from "../views/RandomAnime";
+import RandomManga from "../views/RandomManga";
 
 const BrowserRouter = () => {
     return(
@@ -13,6 +15,9 @@ const BrowserRouter = () => {
             <Route path='/anime-reccomendations' element={<AnimeRec />} />
             <Route path='/top-manga' element={<TopManga/>} />
             <Route path='/manga-reccomendations' element={<MangaRec />} />
+            <Route path='/random-anime' element={<RandomAnime />} />
+            <Route path='/random-manga' element={<RandomManga />} />
+
       </Routes>
     );
 }
