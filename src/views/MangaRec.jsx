2166f6dba7913f4d8fb2b000443e43cde2c2a155
Andrea <andrea.berrocal95@gmail.com>
2022-08-01 17:31:54 -0700

    format code

--- src/views/MangaRec.jsx
+++ src/views/MangaRec.jsx
@@ -3,13 +3,15 @@ import { useContext, useEffect } from "react";
 import { getData } from "../actions/Actions";
 
 const MangaRec = () => {
-    const {loading, error, data, dispatch } = useContext(DataContext);
+  const { loading, error, data, dispatch } = useContext(DataContext);
 
   useEffect(() => {
     dispatch({ type: "FETCHING" });
     const fetchData = async () => {
       try {
-        const mangaRec = await getData(`https://api.jikan.moe/v4/recommendations/manga`);
+        const mangaRec = await getData(
+          `https://api.jikan.moe/v4/recommendations/manga`
+        );
         dispatch({ type: "FETCHED", payload: mangaRec });
       } catch (error) {
         dispatch({ type: "FETCH_ERROR" });
@@ -21,37 +23,45 @@ const MangaRec = () => {
 
   return (
     <div className="rec-wrapper">
-        <h1>Manga Reccomendations</h1>
-      {loading? <img className="loading" src={require('../loading.webp')} alt='loader' />
-      : !loading && error ? <div>{error}</div> 
-      : data.map((rec, index) => {
-        return (
-          <div key={index}>
-            <div className="rec-container">
-              <div>
-                    <h1>If you liked:</h1>
-                    <img
+      <h1>Manga Reccomendations</h1>
+      {loading ? (
+        <img
+          className="loading"
+          src={require("../loading.webp")}
+          alt="loader"
+        />
+      ) : !loading && error ? (
+        <div>{error}</div>
+      ) : (
+        data.map((rec, index) => {
+          return (
+            <div key={index}>
+              <div className="rec-container">
+                <div>
+                  <h1>If you liked:</h1>
+                  <img
                     src={rec.entry[0].images.jpg.image_url}
                     alt={rec.entry[0].title}
-                    />
-                    <p>{rec.entry[0].title}</p>
+                  />
+                  <p>{rec.entry[0].title}</p>
                 </div>
                 <div>
-                    <h1>...Then you might like:</h1>
-                    <img
+                  <h1>...Then you might like:</h1>
+                  <img
                     src={rec.entry[1].images.jpg.image_url}
                     alt={rec.entry[1].title}
-                    />
-                    <p>{rec.entry[1].title}</p>
+                  />
+                  <p>{rec.entry[1].title}</p>
                 </div>
                 <div>
-                    <p>{rec.content}</p>
-                    <p>Anime Rec by: {rec.user.username}</p>
+                  <p>{rec.content}</p>
+                  <p>Anime Rec by: {rec.user.username}</p>
                 </div>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 };
--- src/views/TopAnime.jsx
+++ src/views/TopAnime.jsx
@@ -1,19 +1,17 @@
 import { useContext, useEffect } from "react";
-import { getData } from '../actions/Actions';
-import AddToList from '../components/AddToList';
-import ViewMoreInfo from '../components/ViewMoreInfo';
+import { getData } from "../actions/Actions";
+import AddToList from "../components/AddToList";
+import ViewMoreInfo from "../components/ViewMoreInfo";
 import { DataContext } from "../context/DataContext";
 
-
 const AllAnime = () => {
+  const { loading, error, data, dispatch } = useContext(DataContext);
 
-    const { loading, error, data, dispatch } = useContext(DataContext);
-
-    useEffect(() => {
+  useEffect(() => {
     dispatch({ type: "FETCHING" });
     const fetchData = async () => {
       try {
-        const allAnime = await getData(`https://api.jikan.moe/v4/top/anime`);;
+        const allAnime = await getData(`https://api.jikan.moe/v4/top/anime`);
         dispatch({ type: "FETCHED", payload: allAnime });
       } catch (error) {
         dispatch({ type: "FETCH_ERROR" });
@@ -21,23 +19,32 @@ const AllAnime = () => {
     };
     fetchData();
     console.log("i am used once");
-    console.log(data)
-    }, [])
+    console.log(data);
+  }, []);
 
-    return(
-        <div className="container">
-                {loading? <img className="loading" src={require('../loading.webp')} alt='loader' /> 
-                : !loading && error? <div>{error}</div> 
-                : data.map((anime) => {
-                    return (
-                    <div key={anime.mal_id} className="card-container">
-                        <img src={anime.images.jpg.image_url} alt={anime.title} />
-                        <h1>{anime.title}</h1>
-                        <AddToList />
-                        <ViewMoreInfo />
-                    </div>
-                )})}
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      {loading ? (
+        <img
+          className="loading"
+          src={require("../loading.webp")}
+          alt="loader"
+        />
+      ) : !loading && error ? (
+        <div>{error}</div>
+      ) : (
+        data.map((anime) => {
+          return (
+            <div key={anime.mal_id} className="card-container">
+              <img src={anime.images.jpg.image_url} alt={anime.title} />
+              <h1>{anime.title}</h1>
+              <AddToList />
+              <ViewMoreInfo />
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+};
 export default AllAnime;
