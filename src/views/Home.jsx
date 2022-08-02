49b95d723631b6c784f66ad226f9cb508297f412
Andrea <andrea.berrocal95@gmail.com>
2022-08-01 21:25:49 -0700

    added more code to homepage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,12 +3,30 @@ import RecsAndRevs from "../components/RecsAndRevs";
 
 const Home = () => {
   return (
-    <div>
-      <TopFive url={`https://api.jikan.moe/v4/top/anime`} />
-      <TopFive url={`https://api.jikan.moe/v4/top/manga`} />
-      <RecsAndRevs url={`https://api.jikan.moe/v4/recommendations/anime`} />
-      <RecsAndRevs url={`https://api.jikan.moe/v4/recommendations/manga`} />
-    </div>
+    <main>
+      <section className="intro">
+        <div className="introContainer">
+          <h1>Welcome to Anime City</h1>
+          <p>Where you can browse through popular anime and manga</p>
+          <div className="rndbtn">
+            <button className="randomAnime">Random Anime</button>
+            <button className="randomManga">Random Manga</button>
+          </div>
+        </div>
+      </section>
+      <section className='animeSection'>
+        <TopFive url={`https://api.jikan.moe/v4/top/anime`} />
+      </section>
+      <section className='mangaSection'>
+        <TopFive url={`https://api.jikan.moe/v4/top/manga`} />
+      </section>
+      <section className='animeSection'>
+        <RecsAndRevs url={`https://api.jikan.moe/v4/recommendations/anime`} />
+      </section>
+      <section className='animeSection'>
+        <RecsAndRevs url={`https://api.jikan.moe/v4/recommendations/manga`} />
+      </section>
+    </main>
   );
 };
 export default Home;
