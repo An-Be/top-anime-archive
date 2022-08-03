68dc568830d3696e2c0345e0f7b786990b455967
Andrea <andrea.berrocal95@gmail.com>
2022-08-03 13:17:16 -0700

    added login route and removed mangarec/changed animerec to animerev

--- src/components/BrowserRouter.jsx
+++ src/components/BrowserRouter.jsx
@@ -2,21 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import TopManga from "../views/TopManga";
 import TopAnime from "../views/TopAnime";
 import Home from "../views/Home";
-import AnimeRec from "../views/AnimeRec";
-import MangaRec from "../views/MangaRec";
+import AnimeRev from "../views/AnimeRev";
 import RandomAnime from "../views/RandomAnime";
 import RandomManga from "../views/RandomManga";
+import SeasonalAnime from "../views/SeasonalAnime";
+import Login from "../views/Login";
 
 const BrowserRouter = () => {
     return(
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/top-anime' element={<TopAnime />} />
-            <Route path='/anime-reccomendations' element={<AnimeRec />} />
+            <Route path='/anime-reviews' element={<AnimeRev />} />
             <Route path='/top-manga' element={<TopManga/>} />
-            <Route path='/manga-reccomendations' element={<MangaRec />} />
             <Route path='/random-anime' element={<RandomAnime />} />
             <Route path='/random-manga' element={<RandomManga />} />
+            <Route path='/season-anime' element={<SeasonalAnime />} />
+            <Route path='/login' element={<Login />} />
 
       </Routes>
     );
