7b4007e357d9669b93e1b457fdf0548fb4141631
Andrea <andrea.berrocal95@gmail.com>
2022-08-03 13:19:11 -0700

    added anime review view and seasonalanime view

---
+++ src/views/SeasonalAnime.jsx
@@ -0,0 +1,50 @@
+import { useContext, useEffect } from "react";
+import { DataContext } from "../context/DataContext";
+import { getData } from "../actions/Actions";
+
+const SeasonalAnime = () => {
+    const { loading, animeSeasonData, error, dispatch } = useContext(DataContext);
+
+    useEffect(() => {
+        dispatch({ type: 'FETCHING'})
+        const fetchData = async () => {
+            try{
+                const seasonAnime = await getData(`https://api.jikan.moe/v4/seasons/now`)
+                dispatch({ type: 'FETCHED_SEASON_ANIME', payload: seasonAnime})
+
+            }catch(error){
+                dispatch({ type: 'FETCH_ERROR'})
+            }
+        }
+        fetchData();
+    },[dispatch])
+
+    console.log(animeSeasonData);
+
+    return(
+        <>
+        <h1 style={{textAlign: 'center', marginTop:'6rem'}}>Summer 2022</h1>
+        <div className="container">
+        {loading ? (
+          <img
+            className="loading"
+            src={require("../loading.webp")}
+            alt="loader"
+          />
+        ) : !loading && error ? (
+          <div>{error}</div>
+        ) : (
+            animeSeasonData.map((anime) => {
+            return (
+              <div key={anime.mal_id} className="card-container">
+                <img src={anime.images.jpg.image_url} alt={anime.title} />
+                <h1>{anime.title}</h1>
+              </div>
+            );
+          })
+        )}
+      </div>
+      </>
+    );
+}
+export default SeasonalAnime;
