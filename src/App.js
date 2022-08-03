359f4c6b455e8edf15c03402c48ab330605b9ceb
Andrea <andrea.berrocal95@gmail.com>
2022-08-03 13:16:21 -0700

    added userProvider to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from './components/Header';
 import BrowserRouter from './components/BrowserRouter';
 import DataProvider from './context/DataContext';
+import UserProvider from './context/UserContext';
 
 function App() {
 
   return (
     <div className="App">
       <DataProvider>
+        <UserProvider>
         <Header />
         <BrowserRouter />
+        </UserProvider>
       </DataProvider>
     </div>
   );
