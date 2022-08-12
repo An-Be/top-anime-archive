6aef3443f2eaf0a2f19db0611cf9b732df0fe0d1
Andrea <andrea.berrocal95@gmail.com>
2022-08-11 20:37:29 -0700

    changed styling and prettier extension

--- src/views/TopAnime.jsx
+++ src/views/TopAnime.jsx
@@ -2,40 +2,36 @@ import { useContext, useEffect } from "react";
 import { DataContext } from "../context/DataContext";
 import { UserContext } from "../context/UserContext";
 import { Link } from "react-router-dom";
-import { getDocs, collection} from "firebase/firestore";
+import { getDocs, collection } from "firebase/firestore";
 import { db } from "../firebase.config";
 
 const TopAnime = () => {
   const { loading, error, animeData, dispatch } = useContext(DataContext);
   const { isAuth } = useContext(UserContext);
-  let trendingAnime = []
+  let trendingAnime = [];
 
-  const animeCollectionRef = collection(db, 'Anime');    
+  const animeCollectionRef = collection(db, "Anime");
 
   useEffect(() => {
-    dispatch({ type: 'FETCHING'});
+    dispatch({ type: "FETCHING" });
     const getAnime = async () => {
-      try{
+      try {
         const data = await getDocs(animeCollectionRef);
-        data.docs.map((doc) => 
-        {
+        data.docs.map((doc) => {
+          return trendingAnime.push({ ...doc.data(), doc_id: doc.id });
+        });
 
-          return trendingAnime.push({...doc.data(), doc_id: doc.id})
-        })
-
-        dispatch({ type: "FETCHED_ANIME", payload: trendingAnime});
-    }catch(error){
-        console.log('error', error)
-    }
-    }
+        dispatch({ type: "FETCHED_ANIME", payload: trendingAnime });
+      } catch (error) {
+        dispatch({ type: "FETCH_ERROR"})
+        console.log("error", error);
+      }
+    };
     getAnime();
-  }, [dispatch])
-
-
-
+  }, [dispatch]);
 
   return (
-    <div style={{marginTop: '7rem'}} className="container">
+    <div style={{ marginTop: "7rem" }} className="container">
       {loading ? (
         <img
           className="loading"
@@ -48,9 +44,21 @@ const TopAnime = () => {
         animeData.map((anime) => {
           return (
             <div key={anime.id} className="card-container">
-              <img className="posterImage" src={anime.posterImage.original} alt={anime.canonicalTitle} />
+              <img
+                className="posterImage"
+                src={anime.posterImage.original}
+                alt={anime.canonicalTitle}
+              />
               <h1>{anime.canonicalTitle}</h1>
-              {isAuth && <Link className="addBtn" to='/add-to-anime-list' state={{ id: anime.id}}>Add</Link> }
+              {isAuth && (
+                <Link
+                  className="addBtn"
+                  to="/add-to-anime-list"
+                  state={{ id: anime.id }}
+                >
+                  Add
+                </Link>
+              )}
             </div>
           );
         })
